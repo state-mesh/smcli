5a1e502314a3543d6b42523fcec365c4a0e9f604
dan.chirila <dan.chirila@m-ware.eu>
2025-02-22 17:13:14 +0200

    Build and Publish to APT

---
+++ upload-s3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Uncomment for build
+#npm run build
+#oclif pack deb
+
+
+aptly publish drop squeeze s3:smcli:release-1
+aptly repo drop smcli-release
+
+aptly repo create -distribution=squeeze -component=main smcli-release
+aptly repo add smcli-release ./dist/deb/
+aptly publish repo smcli-release s3:smcli:release-1
+gpg --export --armor > ./apt/release.key
+
+aws s3 sync ./apt s3://smcli/
+
+exit
+
